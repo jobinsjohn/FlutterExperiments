a5d0a7bb80a42e5e7e8dfaaf2c6c1910a8da2f7d
Jobins John <jobins666@gmail.com>
2019-02-19 10:02:04 +0400

    Title changed

--- HelloWorld/flutter_app/lib/main.dart
+++ HelloWorld/flutter_app/lib/main.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    new MaterialApp(
-      title: "Friendlychat",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Chat Application"),
-        ),
-      ),
-    ),
-  );
-}
+void main() = runApp()
+
+//void main() {
+//  runApp(
+//    new MaterialApp(
+//      title: "Friendlychat",
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: new Text("Demo App"),
+//        ),
+//      ),
+//    ),
+//  );
+//}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chat Application',
+      title: 'Demo App',
       theme: ThemeData(
         // This is the theme of your application.
         //
